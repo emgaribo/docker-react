bc9f452d2ff1fd0d1bd30a3f1011678304050635
Eduardo Mendoza <eduardo.garibo@wizeline.com>
2021-03-04 16:12:38 -0600

    Initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Docker Course", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Docker Course/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test("renders Docker Course 2", () => {
+  render(<App />);
+  const linkElement = screen.getByText(/Docker Course/i);
   expect(linkElement).toBeInTheDocument();
 });
